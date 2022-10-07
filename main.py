ce93b59f306c76a0f4a02ffb700dcc50e39a9e95
qinjinfeng <qinjinfeng7@outlook.com>
2022-10-07 16:35:38 +0800

    删除report文件夹下的内容

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pytest
-import os
+# import os
 
 
 if __name__ == "__main__":
     pytest.main()
     # 生成本地allure报告
-    os.system("allure generate ./report/temp -o ./report/allure -c")
+    # os.system("allure generate ./report/allure_result -o ./report/allure_report -c")
     # 打开本地报告
     # os.system("allure open ./report/allure")
